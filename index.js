733fb31b562bc3352da034304ff788b97a2b2ab8
ksykes <kaitlyn.sykes@me.com>
2018-12-16 16:15:09 -0500

    fifth deploy

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const server = require('./api/server')
 const mongoose = require('mongoose')
 
 const { PORT, DB_URI } = require('./api/utils/constants')
-const MONGOOSE_URI = DB_URI || config.MONGODB_URI;
-const API_PORT = PORT|| config.PORT;
+const MONGOOSE_URI = config.MONGODB_URI || DB_URI;
+const API_PORT = config.PORT || PORT;
 
 server.listen(API_PORT, async () => {
   await mongoose.connect(MONGOOSE_URI);
