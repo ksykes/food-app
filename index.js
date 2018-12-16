8da39dc5b25f248d75d0e8be2d4294810f2613c2
ksykes <kaitlyn.sykes@me.com>
2018-12-16 16:00:25 -0500

    third deploy

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const server = require('./api/server')
 const mongoose = require('mongoose')
 
-const { PORT, DB_URI } = require('./api/utils/constants')
+const { PORT } = require('./api/utils/constants')
+const MONGOOSE_URI = process.env.MONGODB_URI || config.MONGODB_URI;
 
 server.listen(PORT, async () => {
-    await mongoose.connect(DB_URI)
+    await mongoose.connect(MONGOOSE_URI)
     console.log(`App listening on port ${PORT}`)
 })
