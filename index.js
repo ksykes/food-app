6e4d980c771d1b996c15a3ebd0c4e107e749dede
ksykes <kaitlyn.sykes@me.com>
2018-12-16 16:21:13 -0500

    talia is not confident

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const server = require('./api/server')
 const mongoose = require('mongoose')
 
-const { PORT, DB_URI } = require('./api/utils/constants')
-const MONGOOSE_URI = config.MONGODB_URI || DB_URI;
-const API_PORT = config.PORT || PORT;
+// const { PORT, DB_URI } = require('./api/utils/constants')
+const MONGOOSE_URI = process.env.DB_URI || config.MONGODB_URI; ;
+const API_PORT = process.env.PORT || config.PORT;
 
 server.listen(API_PORT, async () => {
   await mongoose.connect(MONGOOSE_URI);
